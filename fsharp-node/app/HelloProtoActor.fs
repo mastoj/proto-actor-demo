ca2fd3c91b088286475d9e61df3ebf37ba01ac96
Tomas Jansson <tomas.jansson@unacast.com>
2018-01-24 17:51:57 +0100

    fix some readability

--- fsharp-node/app/HelloProtoActor.fs
+++ fsharp-node/app/HelloProtoActor.fs
@@ -6,8 +6,11 @@ let hello() =
         Actor.create (printfn "Hello from actor1: %A") 
         |> Actor.initProps 
         |> Actor.spawn
+
     let pid2 = 
-        Actor.create (fun x -> printfn "Hello from actor2: %A" x; (sprintf "I was called with %A" x) >! pid1) 
+        Actor.create (fun x -> 
+                printfn "Hello from actor2: %A" x
+                (sprintf "I was called with %A" x) >! pid1) 
             |> Actor.initProps 
             |> Actor.spawn
 
